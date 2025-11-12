6a2d1ebb2dc2847aa95ffbebde677857681adfe9
Rays Edutech Private Limited <164146940+rays-project@users.noreply.github.com>
2025-11-12 13:05:53 +0530

    Update Header.jsx

--- collegeSewa/src/components/header/Header.jsx
+++ collegeSewa/src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "./Header.css";
+import "./header.css";
 import { Link } from "react-router-dom";
 
 const Header = () => {
